dcfc4575182468510d5be7910237ef89f86a2cc5
Matsvei Levin <matsvei.levin@gmail.com>
2022-01-06 19:11:13 +0300

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,58 @@
+var express = require('express');
+var router = express.Router();
+const dataPath = './data/index.json'
+const fs = require('fs');
+
+router.get('/', function (req, res, next) {
+  const accounts = getAccountData()
+  res.send(accounts);
+});
+
+router.post('/', (req, res) => {
+
+  var existAccounts = getAccountData();
+  const newAccountId = Math.floor(100000 + Math.random() * 900000)
+  existAccounts[newAccountId] = req.body
+
+  saveAccountData(existAccounts);
+  res.send({
+    success: true,
+    msg: 'account added successfully'
+  })
+});
+
+router.delete('/', (req, res) => {
+
+  var existAccounts = getAccountData();
+  delete existAccounts[req.body.id]
+
+  saveAccountData(existAccounts);
+  res.send({
+    success: true,
+    msg: 'dataset deleted successfully'
+  })
+});
+
+router.put('/', (req, res) => {
+
+  var existAccounts = getAccountData();
+  let id = Object.keys(req.body)[0];
+  existAccounts[id] = req.body[id];
+  saveAccountData(existAccounts);
+  res.send({
+    success: true,
+    msg: 'dataset updated successfully'
+  })
+});
+
+const saveAccountData = (data) => {
+  const stringifyData = JSON.stringify(data)
+  fs.writeFileSync(dataPath, stringifyData)
+}
+
+const getAccountData = () => {
+  const jsonData = fs.readFileSync(dataPath)
+  return JSON.parse(jsonData)
+}
+
+module.exports = router;
